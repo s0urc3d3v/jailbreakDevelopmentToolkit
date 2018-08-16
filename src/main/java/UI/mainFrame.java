206044c5378451a6243ab4d223a897589ffa2d3b
s0urc3d3v3l0pm3nt <github@techredesigned.com>
2018-08-16 10:35:09 -0400

    init

---
+++ src/main/java/UI/mainFrame.java
@@ -0,0 +1,13 @@
+package UI;
+
+import javax.swing.*;
+
+public class mainFrame { //lol
+    JFrame frame = new JFrame("Toolkit, version " + util.prefs.getVERSION_NUMBER());
+    JPanel panel = new JPanel();
+
+    private void init() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+    }
+}
---
+++ src/main/java/util/prefs.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.prefs.Preferences;
+
+public class prefs {
+    //loaded a runtime with constants
+    private static double VERSION_NUMBER = -1; //-1 is not yet init
+    private final String PREFS_FILE = "resources/preferances.tsv";
+    private static FileWriter writer;
+    private static BufferedReader reader;
+    private static HashMap<String, String> PREFERENCE_DICTIONARY;
+    public prefs() {
+        PREFERENCE_DICTIONARY = new HashMap<>();
+
+        try {
+            writer = new FileWriter(PREFS_FILE, true);
+        } catch (IOException ioException){
+            System.err.println(ioException.getMessage());
+        }
+
+        try{
+            reader = new BufferedReader(new FileReader(PREFS_FILE));
+            String ln = "";
+            while ((ln = reader.readLine()) != null){
+                String regexArray[] = ln.split("\t");
+                PREFERENCE_DICTIONARY.put(regexArray[0], regexArray[1]);
+            }
+        } catch (IOException ioe){
+            ioe.getMessage();
+        }
+    }
+
+    public static double getVERSION_NUMBER() {
+        return Double.valueOf(PREFERENCE_DICTIONARY.get("version"));
+
+    }
+}
