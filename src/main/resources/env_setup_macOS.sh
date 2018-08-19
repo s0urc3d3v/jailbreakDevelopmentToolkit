44f79a17c3757c8d96a783e4e7788e83f387ae4a
s0urc3 <github@techredesigned.com>
2018-08-19 14:38:46 -0400

    fix potential path problem

--- src/main/resources/env_setup_macOS.sh
+++ src/main/resources/env_setup_macOS.sh
@@ -6,8 +6,8 @@ echo "Installing homebrew"
 echo "Installing ldid and xz via homebrew"
 brew install ldid xz
 echo "Adding $THEOS to .bash_profile"
-"THEOS=~/theos" >> .bash_profile
-source .bash_profile
+"THEOS=~/theos" >> ~/.bash_profile
+source ~/.bash_profile
 echo "Downloading theos, this may take a while depending on your internet connection"
 git clone --recursive https://github.com/theos/theos.git ~/theos
 
